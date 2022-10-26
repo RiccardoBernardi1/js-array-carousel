19acffff50b5518cec7a8779cdb713a172c57521
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-26 16:10:51 +0200

    Aggiunge inizio algoritmo js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 "use strict"
-//1. 
+// 1. Creare un array contenente le immagini
+// 2. Creare un ciclo che inserisca le immagini nell'html
+// 3. Nascondere tutte le immagini tranne la prima
+// 4. Aggiungere funzionamento frecce per cambiare immagine
+const imgArray = [];
+
+
+console.log(imgArray)
+for (let i = 0; i < imgArray.length; i++) {
+}
