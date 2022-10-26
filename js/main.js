a477b19573d1a000eb7afdfdc9302e87db64dc11
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-26 19:28:40 +0200

    Aggiunge funzionamento bottoni e bonus

--- js/main.js
+++ js/main.js
@@ -5,17 +5,50 @@
 // 4. Aggiungere funzionamento frecce per cambiare immagine
 const imgArray = [`img/01.jpg`,`img/02.jpg`,`img/03.jpg`,`img/04.jpg`,`img/05.jpg`,];
 console.log(imgArray);
+const container=document.createElement("div");
+container.classList.add("container");
+document.body.append(container);
 const btnUp= document.createElement("button");
 btnUp.innerHTML = '<i class="fa-solid fa-chevron-up"></i>';
 const btnDown= document.createElement("button");
+let active =0;
 btnDown.innerHTML = '<i class="fa-solid fa-chevron-down"></i>';
+let items= 0;
 for (let i = 0; i < imgArray.length; i++) {
-    document.querySelector(".container").innerHTML='<div class="item"></div>';
-    document.querySelector(".item").innerHTML='<img src="img/01.jpg" alt=""></img>';
-    document.querySelector("img").src=imgArray[i];
-    document.querySelector(".item").classList.toggle("show");
-    document.querySelector(".item.show").appendChild(btnUp);
-    document.querySelector(".item.show").appendChild(btnDown);
+    const item=document.createElement("div");
+    item.classList.add("item");
+    container.append(item);
+    items = document.querySelectorAll(".item");
+    const image=document.createElement("img");
+    image.classList.add("image");
+    item.append(image);
+    image.src=imgArray[i];
+    items[active].classList.add("show");
     btnUp.classList.add("btn","btn-up");
     btnDown.classList.add("btn","btn-down");
 }
+items[active].append(btnUp);
+items[active].append(btnDown);
+console.log(items)
+btnUp.addEventListener("click",function() {
+    document.querySelector(".item.show").classList.remove("show");
+    if (active==0){
+        active=items.length-1
+    }else{
+        active= active-1;
+    }
+    items[active].classList.add("show");
+    items[active].append(btnUp);
+    items[active].append(btnDown);
+})
+btnDown.addEventListener("click",function() {
+    items[active].classList.remove("show");
+    if (active==items.length-1){
+        active=0;
+    }else{
+        active= active+1;
+    }
+    items[active].classList.add("show");
+    items[active].append(btnUp);
+    items[active].append(btnDown);
+})
