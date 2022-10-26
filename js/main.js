9950c06cd54ed28c85ee1997e1ae278c37ac5622
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-10-26 17:22:27 +0200

    Aggiunge i bottoni

--- js/main.js
+++ js/main.js
@@ -4,11 +4,18 @@
 // 3. Nascondere tutte le immagini tranne la prima
 // 4. Aggiungere funzionamento frecce per cambiare immagine
 const imgArray = [`img/01.jpg`,`img/02.jpg`,`img/03.jpg`,`img/04.jpg`,`img/05.jpg`,];
-
-console.log(imgArray)
+console.log(imgArray);
+const btnUp= document.createElement("button");
+btnUp.innerHTML = '<i class="fa-solid fa-chevron-up"></i>';
+const btnDown= document.createElement("button");
+btnDown.innerHTML = '<i class="fa-solid fa-chevron-down"></i>';
 for (let i = 0; i < imgArray.length; i++) {
     document.querySelector(".container").innerHTML='<div class="item"></div>';
     document.querySelector(".item").innerHTML='<img src="img/01.jpg" alt=""></img>';
     document.querySelector("img").src=imgArray[i];
     document.querySelector(".item").classList.toggle("show");
+    document.querySelector(".item.show").appendChild(btnUp);
+    document.querySelector(".item.show").appendChild(btnDown);
+    btnUp.classList.add("btn","btn-up");
+    btnDown.classList.add("btn","btn-down");
 }
